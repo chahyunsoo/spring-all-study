202bae6e25c914d203c1b5b3134f78a85acd7c20
chahyunsoo <chahs1012@naver.com>
2023-08-09 20:45:57 +0900

    lectureA: Init Project

---
+++ lectureA/src/main/java/spring/lectureA/LectureAApplication.java
@@ -0,0 +1,13 @@
+package spring.lectureA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureAApplication.class, args);
+	}
+
+}
---
+++ lectureA/src/test/java/spring/lectureA/LectureAApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.lectureA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
