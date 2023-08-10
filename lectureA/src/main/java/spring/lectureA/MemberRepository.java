11a2561a05f560b4bd6fab979c7436d5443955dc
chahyunsoo <chahs1012@naver.com>
2023-08-10 22:41:25 +0900

    lectureA: Section 1

---
+++ lectureA/src/main/java/spring/lectureA/MemberRepository.java
@@ -0,0 +1,23 @@
+package spring.lectureA;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findMember(Long id) {
+        Member member = em.find(Member.class, id);
+        return member;
+    }
+
+}
