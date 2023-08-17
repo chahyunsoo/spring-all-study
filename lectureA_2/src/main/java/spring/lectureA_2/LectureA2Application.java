1cce66c73277d502b863624c2f20927da3dfb558
chahyunsoo <chahs1012@naver.com>
2023-08-17 16:56:19 +0900

    lectureA_2: Initial Project(프로젝트 lectureA 새로 초기화)

---
+++ lectureA_2/src/main/java/spring/lectureA_2/LectureA2Application.java
@@ -0,0 +1,13 @@
+package spring.lectureA_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureA2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureA2Application.class, args);
+	}
+
+}
---
+++ lectureA_2/src/test/java/spring/lectureA_2/LectureA2ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.lectureA_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureA2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
