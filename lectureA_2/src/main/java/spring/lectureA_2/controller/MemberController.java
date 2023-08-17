4cd7c5be5db12bf8b0a544e7a377ca1bef06a456
chahyunsoo <chahs1012@naver.com>
2023-08-17 23:56:32 +0900

    lectureA_2: Section 7(회원 등록)

---
+++ lectureA_2/src/main/java/spring/lectureA_2/controller/MemberController.java
@@ -0,0 +1,22 @@
+package spring.lectureA_2.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import spring.lectureA_2.service.MemberService;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+}
