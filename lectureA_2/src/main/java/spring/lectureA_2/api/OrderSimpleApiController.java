207673c712df6087b7a0f85eac159ecb070fa2a0
chahyunsoo <chahs1012@naver.com>
2024-01-03 19:16:23 +0900

    lectureA_2: N+1 문제를 어떻게 해결

--- lectureA_2/src/main/java/spring/lectureA_2/api/OrderSimpleApiController.java
+++ lectureA_2/src/main/java/spring/lectureA_2/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package spring.lectureA_2.api;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.lectureA_2.domain.Address;
 import spring.lectureA_2.domain.Order;
+import spring.lectureA_2.domain.OrderStatus;
 import spring.lectureA_2.repository.OrderRepository;
 import spring.lectureA_2.repository.OrderSearch;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -26,5 +31,31 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); //Lazy 강제 초기화
         }
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER 2개
+        //N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> collect = orders.stream().map(m -> new SimpleOrderDto(m)).collect(Collectors.toList());
+        return collect;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
