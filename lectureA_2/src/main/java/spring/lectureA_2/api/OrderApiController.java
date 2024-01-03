a0b9c8501c96c46deee75e5e065d445cd65d1b46
chahyunsoo <chahs1012@naver.com>
2024-01-03 22:12:47 +0900

    lectureA_2: Collection 조회 최적화, Entity 직접 노출

---
+++ lectureA_2/src/main/java/spring/lectureA_2/api/OrderApiController.java
@@ -0,0 +1,33 @@
+package spring.lectureA_2.api;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.lectureA_2.domain.Order;
+import spring.lectureA_2.domain.OrderItem;
+import spring.lectureA_2.repository.OrderRepository;
+import spring.lectureA_2.repository.OrderSearch;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+//            for (OrderItem orderItem : orderItems) {
+//                orderItem.getItem().getName();
+//            }
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
